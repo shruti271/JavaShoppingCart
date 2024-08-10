cbd144dd6916558d8912422ee59bbe3fbf238142
1kush-patel <kushpatel123.kp14@gmail.com>
2024-08-10 13:49:54 -0400

    Kush - updated quantity and stock logic and product image

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 const Product = ({ product, addToCart }) => {
   const [quantity, setQuantity] = useState(1);
 
@@ -14,26 +13,40 @@ const Product = ({ product, addToCart }) => {
   const handleDecrease = () => {
     setQuantity(prevQuantity => Math.max(1, prevQuantity - 1));
   };
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+  };
 
   return (
     <div className="product">
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <p>Price: ${product.price.toFixed(2)}</p>
-      <p>Stock: {product.stock}</p>
+      <div className="product-description">
+        <div>
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <p>Price: ${product.price.toFixed(2)}</p>
+          <p>Stock: {product.stock}</p>
+        </div>
+        <input type='file' onChange={handleImageChange} ></input>
+        <div className='product-image'>
+          <img height={100} width={100} src={product.image_id} />
+        </div>
+      </div>
       <div className="quantity-controls">
         <button onClick={handleDecrease} disabled={quantity <= 1}>-</button>
-        <input 
-          type="number" 
-          value={quantity} 
-          onChange={handleQuantityChange} 
-          min="1" 
+        <input
+          type="number"
+          value={quantity}
+          onChange={handleQuantityChange}
+          min="1"
           max={product.stock}
         />
-        <button onClick={handleIncrease} disabled={quantity >= product.stock}>+</button>
+        <button className={(product.stock <= 0 || product.stock < quantity) ? 'disabled-class' : 'enabled-class'}
+          onClick={handleIncrease} disabled={quantity >= product.stock}>+</button>
       </div>
-      <button 
-        onClick={() => addToCart(product, quantity)} 
+      <button
+        className={(product.stock <= 0 || product.stock < quantity) ? 'disabled-class' : 'enabled-class'}
+        onClick={() => addToCart(product, quantity)}
         disabled={product.stock === 0}
       >
         {product.stock === 0 ? 'Out of Stock' : 'Add to Cart'}
